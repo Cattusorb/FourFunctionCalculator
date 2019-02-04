e4133b2731acc9d40afb51a680628b0049917004
Cattusorb <magicmachineplaymc@gmail.com>
2019-02-04 16:03:16 -0500

    Created functions for each of the buttons and made them so they preform the proper functions when they are clicked.

--- app/src/main/java/com/example/four_functioncalculator/MainActivity.java
+++ app/src/main/java/com/example/four_functioncalculator/MainActivity.java
@@ -2,12 +2,82 @@ package com.example.four_functioncalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    /** Calculator to Calculate Input */
+    private Calculator calculate =  new Calculator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        updateView();
+    }
+
+    /**
+     * Does an action based on the button/key pressed
+     * @param view - view that received the click
+     */
+    public void buttonClick(View view) {
+        // I know this doesn't look good.
+        if (view.getId() == R.id.clear_all) {
+            calculate.allClear();
+        } else if (view.getId() == R.id.clear) {
+            calculate.clear();
+        } else if (view.getId() == R.id.equal) {
+            calculate.keyPress('=');
+        } else if(view.getId() == R.id.nine) {
+            calculate.keyPress('9');
+        } else if(view.getId() == R.id.eight) {
+            calculate.keyPress('8');
+        } else if(view.getId() == R.id.seven) {
+            calculate.keyPress('7');
+        } else if(view.getId() == R.id.six) {
+            calculate.keyPress('6');
+        } else if(view.getId() == R.id.five) {
+            calculate.keyPress('5');
+        } else if(view.getId() == R.id.four) {
+            calculate.keyPress('4');
+        } else if(view.getId() == R.id.three) {
+            calculate.keyPress('3');
+        } else if(view.getId() == R.id.two) {
+            calculate.keyPress('2');
+        } else if(view.getId() == R.id.one) {
+            calculate.keyPress('1');
+        } else if(view.getId() == R.id.zero) {
+            calculate.keyPress('0');
+        } else if(view.getId() == R.id.decimal) {
+            calculate.keyPress('.');
+        } else if(view.getId() == R.id.plus) {
+            calculate.keyPress('+');
+        } else if(view.getId() == R.id.minus) {
+            calculate.keyPress('-');
+        } else if(view.getId() == R.id.multiply) {
+            calculate.keyPress('*');
+        } else if(view.getId() == R.id.divide) {
+            calculate.keyPress('/');
+        } else {
+            throw new RuntimeException("The button you just pressed does not have a place here...");
+        }
+
+        updateView();
+    }
+
+    private void updateView() {
+        TextView display = findViewById(R.id.output);
+        display.setText(String.format(Locale.US, "%s", calculate.getDisplay()));
+        /**
+         String displayNumber = calculate.getDisplay();
+         String[] numbers = displayNumber.split(".");
+         int n = Integer.parseInt(numbers[1]);
+         if(n == 0) {
+         display.setText(String.format(Locale.US, "%d", numbers[0]));
+         } else {
+         display.setText(String.format(Locale.US, "%d", displayNumber));
+         }*/
     }
 }
